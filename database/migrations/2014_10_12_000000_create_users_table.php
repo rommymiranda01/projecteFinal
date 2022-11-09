7b7d6868509500cf68dc6e23c38fa8086a7769db
Rommy Miranda <rommy.miranda@cirvianum.cat>
2022-11-09 18:59:51 +0100

    commit afegint la bbdd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('rol');
+            $table->double('saldo');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name='admin';
+        $user->password=('123456');
+        $user->email='admin@admin.com';
+        $user->rol='admin';
+        $user->saldo=0;
+        $user->save();
     }
 }
